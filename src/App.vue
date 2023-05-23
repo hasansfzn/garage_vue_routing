972957b0d7a194fcdf042ccf3325104f7bec6cb6
hasansfzn <hasan@softzino.com>
2023-05-23 12:23:35 +0600

    nav_bar added

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,31 @@
 </script>
 
 <template>
-  <nav>
-    <RouterLink
-      to="/"
-      active-class="active"
-      class="router_nav ml-4 text-xl"
-      >Home</RouterLink
-    >
-    <RouterLink
-      to="/about"
-      active-class="active"
-      class="router_nav"
-      >About</RouterLink
-    >
-  </nav>
-  <div>
-    <RouterView />
+  <div class="mx-3">
+    <nav class="flex p-4">
+      <RouterLink
+        to="/"
+        class="mb-2 p-1 font-bold text-xl"
+        >Car Garage BD</RouterLink
+      >
+      <div class="ml-auto mr-0 flex">
+        <RouterLink
+          to="/"
+          active-class="active"
+          class="router_nav mr-4 mb-2"
+          >Home</RouterLink
+        >
+        <RouterLink
+          to="/about"
+          active-class="active"
+          class="router_nav mb-2"
+          >About</RouterLink
+        >
+      </div>
+    </nav>
+    <div>
+      <RouterView />
+    </div>
   </div>
 </template>
 
@@ -26,9 +35,20 @@
   .router_nav {
     padding: 1px;
     text-decoration: none;
+    font-weight: 500;
   }
-  .active,
-  .router_nav:hover {
-    border-bottom: 1px solid green;
+
+  .router_nav::after {
+    content: "";
+    display: block;
+    background: green;
+    height: 2px;
+    width: 0;
+    transition: width 0.3s;
+  }
+
+  .active::after,
+  .router_nav:hover::after {
+    width: 100%;
   }
 </style>
