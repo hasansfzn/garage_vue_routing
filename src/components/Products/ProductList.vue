02628c8b58d1ed3d38a2c9a1c79c232593e10f7a
hasansfzn <hasan@softzino.com>
2023-05-23 17:03:55 +0600

    product list rendered from the json file

---
+++ src/components/Products/ProductList.vue
@@ -0,0 +1,57 @@
+<script setup>
+  import { useCarStore } from "@/stores/useCarStore";
+  let cars = useCarStore();
+  cars.fill();
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-3">
+    <div
+      class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+      v-for="car in cars?.cars"
+      :key="car.id"
+      :car="car"
+    >
+      <a href="#">
+        <img
+          class="rounded-t-lg"
+          :src="car.img"
+          alt=""
+        />
+      </a>
+      <div class="p-5">
+        <a href="#">
+          <h5
+            class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+          >
+            {{ car.name }}
+          </h5>
+        </a>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {{ car.body }}
+        </p>
+        <a
+          href="#"
+          class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+        >
+          Read more
+          <svg
+            aria-hidden="true"
+            class="w-4 h-4 ml-2 -mr-1"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
