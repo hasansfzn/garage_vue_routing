7994f195488020ba42483932d1ff51bfee4067c8
hasansfzn <hasan@softzino.com>
2023-05-24 13:19:57 +0600

    back button intrigrated

--- src/views/ProductDetails.vue
+++ src/views/ProductDetails.vue
@@ -1,6 +1,8 @@
 <script setup>
   import ProductList from "@/components/Products/ProductList.vue";
   import { useCarStore } from "@/stores/useCarStore";
+  import { useRouter } from "vue-router";
+  const router = useRouter();
   import { computed } from "vue";
   let cars = useCarStore();
   cars.fill();
@@ -30,6 +32,7 @@
       </h3>
       <!-- go back button -->
       <button
+        @click="router.back()"
         type="button"
         class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 my-2"
       >
