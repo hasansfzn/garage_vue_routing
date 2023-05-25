ac4bd5cd6970c6c34721bb5a31f7cb6787de146d
hasansfzn <hasan@softzino.com>
2023-05-25 12:37:58 +0600

    added about background

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,25 +1,58 @@
 <script setup></script>
 
 <template>
-  <div>
-    <h2 class="text-6xl my-4 font-semibold text-green-500 md:w-2/3">
-      Car in our store is much more affortable, reliable, trustable
-    </h2>
-    <div class="flex place-content-start">
-      <div class="space-y-4 w-11/12 md:w-10/12 lg:w-3/4">
-        <h2 class="text-5xl">We are the one to serve you well.</h2>
-        <p class="text-lg text-justify">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero illo
-          assumenda itaque sequi sed quisquam minima rerum tenetur aut rem,
-          dignissimos qui veritatis corrupti odio, vitae voluptate tempora alias
-          dicta. Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-          Veritatis quos praesentium vel laborum recusandae adipisci, enim illum
-          eum sit illo asperiores quis sint, earum error dolores nam eligendi
-          officia perferendis.
-        </p>
+  <section class="aboutBannerSection">
+    <div class="content flex items-center justify-end text-right">
+      <div class="content-items w-4/5 p-4">
+        <h2 class="text-6xl my-4 font-semibold text-green-500">
+          Car in our store is much more affortable, reliable, trustable
+        </h2>
+        <div class="space-y-4">
+          <h2 class="text-5xl">We are the one to serve you well.</h2>
+          <p class="text-lg text-justify w-3/4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero illo
+            assumenda itaque sequi sed quisquam minima rerum tenetur aut rem,
+            dignissimos qui veritatis corrupti odio, vitae voluptate tempora
+            alias dicta. Lorem ipsum dolor sit, amet consectetur adipisicing
+            elit. Veritatis quos praesentium vel laborum recusandae adipisci,
+            enim illum eum sit illo asperiores quis sint, earum error dolores
+            nam eligendi officia perferendis.
+          </p>
+        </div>
       </div>
     </div>
-  </div>
+  </section>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .aboutBannerSection {
+    display: relative;
+    background-image: url(../assets/carAbout.jpg);
+    background-size: cover;
+    background-position: center;
+    height: 80vh;
+  }
+
+  .content {
+    /* position: absolute; */
+    /* top: 0;
+    left: 0; */
+    width: 100%;
+    height: 100%;
+    background-color: rgba(
+      0,
+      0,
+      0,
+      0.5
+    ); /* Adjust opacity by modifying the last value (0.5 in this example) */
+    /* display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center; */
+    /* 
+    text-align: end;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end; */
+  }
+</style>
